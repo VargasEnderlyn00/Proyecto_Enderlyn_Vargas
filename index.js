344552f9e5cce17fed99dd1ab96720a17aa175df
VargasEnderlyn00 <enderlynelienai.vargas017@gmail.com>
2024-01-25 20:35:33 -0400

    Contador y verifica si es par o impar

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 console.log("Hola mundo");
+
+console.log("Los numeros del 1 al 20");
+ 
+
+
+
+for (let index = 1; index <= 20; index++) {
+    
+
+    if(index % 2===0){
+
+    console.log(index + "Par");   
+
+    }else{
+        console.log(index + "Impar");
+    }
+    
+}
