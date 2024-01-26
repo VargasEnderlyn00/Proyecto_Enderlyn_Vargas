0b830aefa77c8eeac13d98bb22b065141a65a877
VargasEnderlyn00 <enderlynelienai.vargas017@gmail.com>
2024-01-25 23:05:51 -0400

    Cambios en el HTML/ JavaScript/ README/ agregado Css

--- README.md
+++ README.md
@@ -3,6 +3,9 @@
 En este repositorio se encuentra el segundo proyecto de Espacio EDUCA, donde 
 utilizamos Node.js y Git
 
+--Este proyecto es un contador, que le mostrara los numeros del 1 al 20 y en donde el verificara cual es par o impar
+
 Instruciones del proyecto
 
--Ejecutar npm start
+-Ejecutar en un navegador
+-Hacer click en el boton
--- index.js
+++ index.js
@@ -1,19 +1,26 @@
-console.log("Hola mundo");
+function Imprimir(){
 
-console.log("Los numeros del 1 al 20");
- 
-
-
-
-for (let index = 1; index <= 20; index++) {
+    for (let index = 1; index <= 20; index++) {
     
-
+    var listItem = document.createElement("li");
+    
     if(index % 2===0){
-
-    console.log(index + "Par");   
+        console.log(index + " Par");   
+        listItem.innerHTML =  index + "_Par";
+             
+        listItem.style.backgroundColor = "#2cbc7b";
+        
+        console.log(index + " Par");   
 
     }else{
-        console.log(index + "Impar");
+        console.log(index + " Impar");
+        listItem.innerHTML =  index + "_Impar";
+            
+        listItem.style.backgroundColor = "#bc2c2c";
+        
+        console.log(index + " Impar");
     }
-    
+    document.getElementById("historial").appendChild(listItem);
+}
+
 }
